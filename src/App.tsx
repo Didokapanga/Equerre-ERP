870ed62501a95304b8cced42ac807c3987c32322
Dido Kapanga <didokapanga@outlook.com>
2025-07-07 11:39:11 +0100

    redirects Commit 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { useAuth } from './hooks/useAuth';
 import { AuthForm } from './components/Auth/AuthForm';
 import { Layout } from './components/Layout/Layout';
@@ -210,18 +211,12 @@ function App() {
   };
 
   return (
-    <Layout currentModule={currentModule} onModuleChange={setCurrentModule}>
-      {renderModule()}
-    </Layout>
+    <Router>
+      <Layout currentModule={currentModule} onModuleChange={setCurrentModule}>
+        {renderModule()}
+      </Layout>
+    </Router>
   );
-
-  // return (
-  //   <Router>
-  //     <Layout currentModule={currentModule} onModuleChange={setCurrentModule}>
-  //       {renderModule()}
-  //     </Layout>
-  //   </Router>
-  // );
 }
 
 export default App;
